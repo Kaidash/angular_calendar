4fdbdb35d5a5c85157c08c9b64bb2f73c8977dca
kaidash <thisissparta85>
2016-09-15 08:35:08 +0300

    events add sort

--- src/js/controllers/HomeCtrl.js
+++ src/js/controllers/HomeCtrl.js
@@ -7,9 +7,9 @@ function HomeCtrl($scope,location) {
 
     $scope.day = moment();
     var json=[
-        { "name": "Задача 2", "date": "2016-08-28", time: "15:00:00", category: "cat1", VIP: true  },
+        { "name": "Задача 2", "date": "2016-08-28", time: "10:00:00", category: "cat1", VIP: true  },
         { "name": "Задача 1", "date": "2016-08-30", time: "4:00:00", category: "cat2", VIP: true  },
-        { "name": "Задача 3", "date": "2016-08-28", time: "16:00:00", category: "cat1", VIP: true  }
+        { "name": "Задача 3", "date": "2016-08-28", time: "9:00:00", category: "cat1", VIP: true  }
     ];
     $scope.ocw=json
 
